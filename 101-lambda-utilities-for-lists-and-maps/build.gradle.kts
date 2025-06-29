95921cd896e0931241f2fdc4069106122d75a5c0
DanySK <danilo.pianini@gmail.com>
2025-06-29 18:05:18 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@8f65ce93da42325ad5a2a940a1afa8cf0d02ef1f 🚀

--- 101-lambda-utilities-for-lists-and-maps/build.gradle.kts
+++ 101-lambda-utilities-for-lists-and-maps/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     application
-    id("org.danilopianini.gradle-java-qa") version "1.116.0"
+    id("org.danilopianini.gradle-java-qa") version "1.123.0"
 }
 
 tasks.javadoc {
